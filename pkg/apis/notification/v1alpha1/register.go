af4f90bbc880234937ec7c763b1dfbc241ff1559
Jose Szychowski <joseszychowski@gmail.com>
2026-03-24 09:16:59 -0300

    feat: Introduce ContactGroupEnrollmentPolicy and controller to automate contact group memberships.

--- pkg/apis/notification/v1alpha1/register.go
+++ pkg/apis/notification/v1alpha1/register.go
@@ -33,6 +33,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&EmailBroadcastList{},
 		&ContactGroupMembershipRemoval{},
 		&ContactGroupMembershipRemovalList{},
+		&ContactGroupEnrollmentPolicy{},
+		&ContactGroupEnrollmentPolicyList{},
 	)
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil
